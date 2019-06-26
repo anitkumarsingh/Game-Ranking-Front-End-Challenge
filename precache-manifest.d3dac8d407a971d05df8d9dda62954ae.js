e993872fc816ba43bb82c968b3eab381a33e8a88
Anit Kumar <anikumarsingh99@gmail.com>
2019-06-26 13:28:44 +0530

    Updates

---
+++ precache-manifest.d3dac8d407a971d05df8d9dda62954ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5a8230672515e407dd31e798c6e691",
+    "url": "/Game-Ranking/index.html"
+  },
+  {
+    "revision": "0f9535de3796696d7c28",
+    "url": "/Game-Ranking/static/js/2.51de7a9a.chunk.js"
+  },
+  {
+    "revision": "25c0bf14f7eb1f9e3200",
+    "url": "/Game-Ranking/static/js/main.fa2ab03a.chunk.js"
+  },
+  {
+    "revision": "3fb4e02f490f8600b230",
+    "url": "/Game-Ranking/static/js/runtime~main.938fca4e.js"
+  }
+]);
